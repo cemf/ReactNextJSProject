fc56b62375584e1c5f18bfedc8d4d83d4c3d427a
Carlos <kaikelmfabricio@gmail.com>
2024-10-10 21:05:12 -0300

    test: putting mock on homes tests

--- src/templates/Home/Home.test.jsx
+++ src/templates/Home/Home.test.jsx
@@ -1,10 +1,58 @@
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import { Home } from '.';
 
-describe('<Home />',() =>{
-  it('should be a dummy test', () => {
-    expect(1).toBe(1);
+const handlers = [
+  rest.get('https://jsonplaceholder.typicode.com/posts', async (req, res, ctx) => {
+    return res(
+      ctx.json([
+        {
+          userId: 1,
+          id: 1,
+          title: 'title1',
+          body: 'body1',
+          url: 'img1.jpg',
+        },
+        {
+          userId: 2,
+          id: 2,
+          title: 'title2',
+          body: 'body2',
+          url: 'img1.jpg',
+        },
+        {
+          userId: 3,
+          id: 3,
+          title: 'title3',
+          body: 'body3',
+          url: 'img3.jpg',
+        },
+      ]),
+    );
+  }),
+];
+
+const server = setupServer(...handlers);
+
+describe('<Home />', () => {
+
+  beforeAll(()=>{
+    server.listen();
+  })
+
+  afterEach(()=>{
+    server.resetHandlers();
+  })
+
+  afterAll(()=>{
+    server.close();
+  })
+
+  it('should render search, posts and load more', async () => {
+    const { debug } = render(<Home />);
+    const noMorePosts = screen.getByText('Não existem posts =(');
+    await waitForElementToBeRemoved(noMorePosts);
+    debug();
   });
-  
-})
-
-
-
+});
